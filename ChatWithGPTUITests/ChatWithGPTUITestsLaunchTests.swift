0cfb8ae99e1e137deddc109b5799a839d93d3ae7
Daniel Leivers <dan@otaku-dev.co.uk>
2024-01-15 12:00:31 +0000

    Initial Commit

---
+++ ChatWithGPTUITests/ChatWithGPTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChatWithGPTUITestsLaunchTests.swift
+//  ChatWithGPTUITests
+//
+//  Created by Daniel Leivers on 15/01/2024.
+//
+
+import XCTest
+
+final class ChatWithGPTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
