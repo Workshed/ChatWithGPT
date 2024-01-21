eeec65be9b34a48db895e14579cd6b013f884cf4
Daniel Leivers <dan@otaku-dev.co.uk>
2024-01-21 08:27:40 +0000

    Removed unnecessary SceneDelegate

--- ChatWithGPT/AppDelegate.swift
+++ ChatWithGPT/AppDelegate.swift
@@ -10,16 +10,21 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var coordinator: Coordinator?
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let navController = UINavigationController()
+
+        coordinator = AppCoordinator(navigationController: navController)
+        coordinator?.start()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
